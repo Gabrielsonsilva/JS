3dac54cf9516d57e4fa798037dd6aa8c5ee7dc06
Gabrielsonsilva <gabrielsonsilva14@gmail.com>
2023-11-09 22:25:02 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# JS
+ Aulas de JavaScript com o Gustavo Guanabara
---
+++ aula11/ex001.js
@@ -0,0 +1,10 @@
+console.log('O console funcionol corretamente! ')
+
+var vel = 70.5
+console.log(`A Velocidade do seu carro é ${vel}Km/h`)
+
+if(vel > 60) { //Condição simples!
+    console.log(`Sua velocidade ultrapasse o permitido. MULTADO!`)
+}
+
+console.log(`Diriga sempre usando cinto de segurança!`)
