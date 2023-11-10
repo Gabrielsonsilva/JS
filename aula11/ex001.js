1481aacb039ceca7455e2c917c11bfcbbe5dc882
Gabrielsonsilva <gabrielsonsilva14@gmail.com>
2023-11-09 22:27:00 -0300

    Update ex001.js

--- aula11/ex001.js
+++ aula11/ex001.js
@@ -3,7 +3,7 @@ console.log('O console funcionol corretamente! ')
 var vel = 70.5
 console.log(`A Velocidade do seu carro é ${vel}Km/h`)
 
-if(vel > 60) { //Condição simples!
+if(vel > 60) { //Condição simples!!
     console.log(`Sua velocidade ultrapasse o permitido. MULTADO!`)
 }
 
